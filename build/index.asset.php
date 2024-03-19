6f81852aa81e4ce3f1b5f38075e596148ab0bd88
colinduwe <colinduwe@gmail.com>
2024-03-19 15:28:04 -0700

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'dba741bf949e10fae83b');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '9d535972d509895e6b31');
